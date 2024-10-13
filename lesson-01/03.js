06c8a1c2d8e64307ad60dbaef8e921ce1d6a844b
Anna6463 <tvoianna@gmail.com>
2024-10-13 13:19:00 +0300

    task_3 change code

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -1,17 +1,25 @@
 
 
+// let number = 1;
+// console.log(number); 
+
+
+// ++number;  
+// ++number;  
+// console.log(number); 
+
+
+// console.log(number); 
+
+
+// --number;  
+// --number;  
+// console.log(number); 
+
+
 let number = 1;
-console.log(number); 
 
-
-++number;  
-++number;  
-console.log(number); 
-
-
-console.log(number); 
-
-
---number;  
---number;  
-console.log(number); 
+console.log(number++); // 1 (постинкремент, сначала выводится 1, потом number увеличивается на 1)
+console.log(++number); // 3 (прединкремент, сначала number становится 3, затем выводится 3)
+console.log(number--); // 3 (постинкремент, сначала выводится 3, потом number увеличивается на 1)
+console.log(--number); // 1 (прединкремент, number уменьшается до 1, затем выводится 1)
