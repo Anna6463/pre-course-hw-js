bc81b3c96da96d8d47110614f16250461a885599
Anna6463 <tvoianna@gmail.com>
2024-10-15 15:03:36 +0300

    lesson2_task6_6

--- lesson-02/06.js
+++ lesson-02/06.js
@@ -13,6 +13,7 @@ let passportCopy = JSON.parse(JSON.stringify(passportWithAddress));
 passportCopy.address.city = 'Bobryisk';
 
 // Проверяем, что изменения верны
+console.log(passportWithAddress);
 console.log(passportCopy);
 
-console.log(passportWithAddress);
+
